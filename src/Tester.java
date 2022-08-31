f2bcd1697b8b51ba599333f4014249df1bfd54e1
JackBurghardt <jackburghardt@yahoo.com>
2022-08-31 11:22:43 -0700

    Finished Tester
    Everything works and all the base cases are fine

--- src/Tester.java
+++ src/Tester.java
@@ -7,10 +7,27 @@ public class Tester {
 	WF.openAccount (012);
 	
 	WF.openAccount (013, 100.0);
-	//check to see if balances work and if intialization is correct
+	//check to see if balances work and if initialization is correct
 	System.out.println(WF.checkBalance(012)); //should be 0.0
 	
 	System.out.println(WF.checkBalance (013)); // should be 100.0
 	
+	//check if deposit works
+	WF.depositMoney(012, 100);
+	WF.depositMoney(002, 0);//shouldn't deposit money
+	
+	System.out.println(WF.checkBalance(012)); // should be 100
+	
+	WF.withdrawMoney (012, 10);
+	System.out.println(WF.checkBalance(012));//
+	
+	WF.withdrawMoney(012, -100);//shouldn't withdraw
+	
+	WF.withdrawMoney(012, 99); //shouldn't withdraw
+	System.out.println(WF.checkBalance(012));
+	
+	//check rounding 
+	WF.withdrawMoney(012, 20.2);
+	System.out.println(WF.checkBalance(012)); // Should be 69.8 
 	}
 }
